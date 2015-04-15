bca6955b89cd0d0b1afe9aefd27c007e4489fd3d
jcustodio9 <jcustodio@planit.net.au>
2015-04-15 11:03:58 +0800

    v 0.6

---
+++ batch_files/chrome/navigation.bat
@@ -0,0 +1,3 @@
+@echo off
+Call cd C:\automation\rac_factory_selenium
+Call rake navigation_pc BROWSER=chrome URL=https://rac.com.au
---
+++ batch_files/chrome/travel_insurance_quote.bat
@@ -0,0 +1,3 @@
+@echo off
+Call cd C:\automation\rac_factory_selenium
+Call rake travel_insurance_quote BROWSER=chrome URL=https://rac.com.au
